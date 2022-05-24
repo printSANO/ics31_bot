ddb2ad1ff90a177517aa58b6c6bc94299cc5361a
printSANO <leehahnwoong@gmail.comm>
2022-05-24 01:00:11 -0700

    [edit bot.py]

--- bot.py
+++ bot.py
@@ -48,16 +48,12 @@ async def on_message(message):
         x = check_time()
         month2 = x[0]
         day2 = x[1]
-        #hour2 = x[2]
-        #minute2 = x[3]
         for i in lines:
             j = i.split(',')
             k = j[0].split("/")
             l = j[2].split(":")
             month1 = k[0]
             day1 = k[1]
-            #hour1 = l[0]
-            #minute1 = l[1]
             if int(month1) >= int(month2):
                 if int(day1) == int(day2)+1:
                     await message.channel.send(f"{j[1]} tomorrow at {j[2]}")
@@ -68,16 +64,12 @@ async def on_message(message):
         x = check_time()
         month2 = x[0]
         day2 = x[1]
-        #hour2 = x[2]
-        #minute2 = x[3]
         for i in lines:
             j = i.split(',')
             k = j[0].split("/")
             l = j[2].split(":")
             month1 = k[0]
             day1 = k[1]
-            #hour1 = l[0]
-            #minute1 = l[1]
             if int(month1) >= int(month2):
                 if int(day1) >= int(day2):
                     await message.channel.send(f"{j[1]} in {j[0]}at {j[2]}")
